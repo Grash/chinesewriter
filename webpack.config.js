3cfe48985135bcb81f03132003720c0ed231cc0e
David Farkas <david_farkas@epam.com>
2019-02-21 08:17:50 +0800

    Add sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const config = {
         path: path.resolve(__dirname, 'www/js'),
         filename: 'index_react.js'
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
